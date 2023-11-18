04c0a7a746e45baf64f5affbc1a7493706e5be5f
Edmond <edmond.santiago7@gmail.com>
2023-11-19 00:01:17 +1100

    add js fetch to db.json

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const productAPI = "http://localhost:3000/products"
+const productSection = document.querySelector("#product-collection")
 
-fetch("http://localhost:3000/products")
-.then(res => res.json())
-.then(data => console.log(data))
+fetch(productAPI)
+  .then(res => res.json())
+  .then(products => renderProducts(products))
+
+function renderProducts(products) {
+  productSection.innerHTML = "";
+  products.forEach(product => renderProduct(product))
+}
+
+function renderProduct(product) {
+  const div = document.createElement("div")
+  div.classList.add = "products"
+  productSection.append(div)
+}
