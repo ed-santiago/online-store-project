085c61259d7ab1b89f26909a4c8416718e38a4db
Edmond <edmond.santiago7@gmail.com>
2023-11-18 19:10:54 +1100

    add CSS rules to header and categories

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-
 
 fetch("http://localhost:3000/products")
 .then(res => res.json())
